14b093694587ba19efbff70ab663c6e34c8dc51f
黎健成 <lijiancheng@lijianchengdeMacBook-Pro.local>
2019-08-17 10:35:37 +0800

    修改页面样式
    修改页面样式

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -2,8 +2,25 @@
 
 Page({
   data: {
-    searchContent:''
+    searchContent: '',
+
+
+    navScrollLeft: 0,
+    currentTab: 0,
+    navData: [{
+      text: '推荐'
+    }, {
+      text: '热门'
+    }]
   },
   onLoad: function () {
+  },
+  switchTab(event) {
+    var cur = event.detail.current;
+    var singleNavWidth = this.data.windowWidth / 5;
+    this.setData({
+      currentTab: cur,
+      navScrollLeft: (cur - 2) * singleNavWidth
+    });
   }
 })
